33f8a139669b046c51e03279595cca49dcd1ede0
Thanh Tung Le <thanhtungle73@gmail.com>
2023-09-10 12:04:52 +0700

    setup source code with Spring Cloud

---
+++ service-registry/src/test/java/site/thanhtungle/serviceregistry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package site.thanhtungle.serviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
