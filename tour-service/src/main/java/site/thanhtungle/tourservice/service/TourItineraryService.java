5b436b5a655e49bb3d14f62d3f4932dbd8095903
Thanh Tung Le <thanhtungle73@gmail.com>
2023-11-11 00:38:24 +0700

    feat(Tour Itinerary): add get, getAll & delete tour itinerary apis

--- tour-service/src/main/java/site/thanhtungle/tourservice/service/TourItineraryService.java
+++ tour-service/src/main/java/site/thanhtungle/tourservice/service/TourItineraryService.java
@@ -1,8 +1,11 @@
 package site.thanhtungle.tourservice.service;
 
 import org.springframework.transaction.annotation.Transactional;
+import site.thanhtungle.commons.model.response.success.PagingApiResponse;
 import site.thanhtungle.tourservice.model.dto.request.touritinerary.TourItineraryRequestDTO;
 import site.thanhtungle.tourservice.model.dto.response.touritinerary.TourItineraryResponseDTO;
+
+import java.util.List;
 
 @Transactional
 public interface TourItineraryService {
@@ -10,4 +13,12 @@ public interface TourItineraryService {
     TourItineraryResponseDTO createTourItinerary(TourItineraryRequestDTO tourItineraryRequestDTO);
 
     TourItineraryResponseDTO updateTourItinerary(Long tourItineraryId,TourItineraryRequestDTO tourItineraryRequestDTO);
+
+    @Transactional(readOnly = true)
+    TourItineraryResponseDTO getTourItinerary(Long tourItineraryId);
+
+    @Transactional(readOnly = true)
+    PagingApiResponse<List<TourItineraryResponseDTO>> getAllTourItinerary(Integer page, Integer pageSize, String sort);
+
+    void deleteTourItinerary(Long tourItineraryId);
 }
