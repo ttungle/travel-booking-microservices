cfa48a0102164d6a83a1751a692f9528ffaad404
Tung Le <thanhtungle73@gmail.com>
2023-11-11 00:39:40 +0700

    feat(Tour Itinerary): add get, getAll & delete tour itinerary apis (#14)

--- tour-service/src/main/java/site/thanhtungle/tourservice/controller/TourItineraryController.java
+++ tour-service/src/main/java/site/thanhtungle/tourservice/controller/TourItineraryController.java
@@ -5,9 +5,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import site.thanhtungle.commons.model.response.success.BaseApiResponse;
+import site.thanhtungle.commons.model.response.success.PagingApiResponse;
 import site.thanhtungle.tourservice.model.dto.request.touritinerary.TourItineraryRequestDTO;
 import site.thanhtungle.tourservice.model.dto.response.touritinerary.TourItineraryResponseDTO;
 import site.thanhtungle.tourservice.service.TourItineraryService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("${api.url.tourItinerary}")
@@ -33,5 +36,29 @@ public class TourItineraryController {
                 tourItineraryId, tourItineraryRequestDTO);
         BaseApiResponse<TourItineraryResponseDTO> response = new BaseApiResponse<>(HttpStatus.OK.value(), responseDTO);
         return ResponseEntity.ok().body(response);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<BaseApiResponse<TourItineraryResponseDTO>> getTourItinerary(
+            @PathVariable("id") Long tourItineraryId) {
+        TourItineraryResponseDTO responseDTO = tourItineraryService.getTourItinerary(tourItineraryId);
+        BaseApiResponse<TourItineraryResponseDTO> response = new BaseApiResponse<>(HttpStatus.OK.value(), responseDTO);
+        return ResponseEntity.ok().body(response);
+    }
+
+    @GetMapping
+    public ResponseEntity<PagingApiResponse<List<TourItineraryResponseDTO>>> getAllTourItinerary(
+            @RequestParam(name = "page", defaultValue = "1") Integer page,
+            @RequestParam(name = "pageSize", defaultValue = "25") Integer pageSize,
+            @RequestParam(name = "sort", required = false) String sort
+    ) {
+        PagingApiResponse<List<TourItineraryResponseDTO>> responseDTOList = tourItineraryService.getAllTourItinerary(page, pageSize, sort);
+        return ResponseEntity.ok().body(responseDTOList);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteTourItinerary(@PathVariable("id") Long tourItineraryId) {
+        tourItineraryService.deleteTourItinerary(tourItineraryId);
     }
 }
--- tour-service/src/main/java/site/thanhtungle/tourservice/service/impl/TourItineraryServiceImpl.java
+++ tour-service/src/main/java/site/thanhtungle/tourservice/service/impl/TourItineraryServiceImpl.java
@@ -1,8 +1,13 @@
 package site.thanhtungle.tourservice.service.impl;
 
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import site.thanhtungle.commons.exception.CustomNotFoundException;
+import site.thanhtungle.commons.model.response.success.PageInfo;
+import site.thanhtungle.commons.model.response.success.PagingApiResponse;
 import site.thanhtungle.tourservice.mapper.TourItineraryMapper;
 import site.thanhtungle.tourservice.model.dto.request.touritinerary.TourItineraryRequestDTO;
 import site.thanhtungle.tourservice.model.dto.response.touritinerary.TourItineraryResponseDTO;
@@ -11,8 +16,10 @@ import site.thanhtungle.tourservice.model.entity.TourItinerary;
 import site.thanhtungle.tourservice.repository.TourItineraryRepository;
 import site.thanhtungle.tourservice.repository.TourRepository;
 import site.thanhtungle.tourservice.service.TourItineraryService;
+import site.thanhtungle.tourservice.util.PageUtil;
 
 import java.security.InvalidParameterException;
+import java.util.List;
 
 @Service
 @AllArgsConstructor
@@ -45,5 +52,36 @@ public class TourItineraryServiceImpl implements TourItineraryService {
 
         TourItinerary savedTourItinerary = tourItineraryRepository.save(tourItinerary);
         return tourItineraryMapper.toTourItineraryResponseDTO(savedTourItinerary);
+    }
+
+    @Override
+    public TourItineraryResponseDTO getTourItinerary(Long tourItineraryId) {
+        if (tourItineraryId == null) throw new InvalidParameterException("Tour itinerary id should not be null.");
+
+        TourItinerary tourItinerary = tourItineraryRepository.findById(tourItineraryId).orElseThrow(
+                () -> new CustomNotFoundException("No tour itinerary found with that id."));
+        return tourItineraryMapper.toTourItineraryResponseDTO(tourItinerary);
+    }
+
+    @Override
+    public PagingApiResponse<List<TourItineraryResponseDTO>> getAllTourItinerary(Integer page, Integer pageSize, String sort) {
+        PageRequest pageRequest = PageUtil.getPageRequest(page, pageSize, sort);
+
+        Page<TourItinerary> tourItineraryListPaging = tourItineraryRepository.findAll(pageRequest);
+        List<TourItinerary> tourItineraryList = tourItineraryListPaging.getContent();
+        List<TourItineraryResponseDTO> tourItineraryResponseDTOList = tourItineraryList.stream()
+                .map(tourItineraryMapper::toTourItineraryResponseDTO)
+                .toList();
+        PageInfo pageInfo = new PageInfo(page, pageSize, tourItineraryListPaging.getTotalElements());
+
+        return new PagingApiResponse<>(HttpStatus.OK.value(), tourItineraryResponseDTOList, pageInfo);
+    }
+
+    @Override
+    public void deleteTourItinerary(Long tourItineraryId) {
+        if (tourItineraryId == null) throw new InvalidParameterException("Tour itinerary id cannot be null.");
+        tourItineraryRepository.findById(tourItineraryId).orElseThrow(
+                () -> new CustomNotFoundException("No tour itinerary found with that id."));
+        tourItineraryRepository.deleteById(tourItineraryId);
     }
 }
