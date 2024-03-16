5f1d659bdd934a100d3255fb2ce9e476bb93896b
Thanh Tung Le <thanhtungle73@gmail.com>
2024-03-17 00:36:31 +0700

    feat(users): add crud user apis with Keycloak admin client

--- account-service/src/main/java/site/thanhtungle/accountservice/mapper/UserMapper.java
+++ account-service/src/main/java/site/thanhtungle/accountservice/mapper/UserMapper.java
@@ -1,20 +1,25 @@
 package site.thanhtungle.accountservice.mapper;
 
 import org.keycloak.representations.AccessTokenResponse;
+import org.keycloak.representations.idm.UserRepresentation;
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
 import org.mapstruct.NullValuePropertyMappingStrategy;
 import site.thanhtungle.accountservice.model.dto.request.RegisterUserRequestDTO;
 import site.thanhtungle.accountservice.model.dto.request.UserRequestDTO;
+import site.thanhtungle.accountservice.model.dto.request.UserUpdateRequestDTO;
 import site.thanhtungle.accountservice.model.dto.response.RegisterUserResponseDTO;
 import site.thanhtungle.accountservice.model.dto.response.UserResponseDTO;
 
 @Mapper(componentModel = "spring", nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
 public interface UserMapper {
 
-    UserResponseDTO toUserResponseDTO(UserRequestDTO userRequestDTO);
+    UserResponseDTO toUserResponseDTO(UserRepresentation userRepresentation);
 
     RegisterUserResponseDTO toRegisterUserResponseDTO(UserRequestDTO userRequestDTO,
                                                       AccessTokenResponse accessTokenResponse);
 
     UserRequestDTO toUserRequestDTO(RegisterUserRequestDTO registerUserRequestDTO);
+
+    UserRepresentation updateUser(UserUpdateRequestDTO userUpdateRequestDTO, @MappingTarget UserRepresentation userRepresentation);
 }
--- account-service/src/main/java/site/thanhtungle/accountservice/service/impl/KeycloakUserServiceImpl.java
+++ account-service/src/main/java/site/thanhtungle/accountservice/service/impl/KeycloakUserServiceImpl.java
@@ -3,6 +3,7 @@ package site.thanhtungle.accountservice.service.impl;
 
 import jakarta.ws.rs.core.Response;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.keycloak.admin.client.CreatedResponseUtil;
 import org.keycloak.admin.client.Keycloak;
 import org.keycloak.admin.client.resource.*;
@@ -11,17 +12,24 @@ import org.keycloak.representations.idm.CredentialRepresentation;
 import org.keycloak.representations.idm.RoleRepresentation;
 import org.keycloak.representations.idm.UserRepresentation;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import site.thanhtungle.accountservice.mapper.UserMapper;
 import site.thanhtungle.accountservice.model.dto.request.UserRequestDTO;
+import site.thanhtungle.accountservice.model.dto.request.UserUpdateRequestDTO;
 import site.thanhtungle.accountservice.model.dto.response.UserResponseDTO;
 import site.thanhtungle.accountservice.service.KeycloakUserService;
 import site.thanhtungle.commons.exception.CustomBadRequestException;
+import site.thanhtungle.commons.exception.CustomNotFoundException;
+import site.thanhtungle.commons.model.response.success.PageInfo;
+import site.thanhtungle.commons.model.response.success.PagingApiResponse;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
+@Slf4j
 public class KeycloakUserServiceImpl implements KeycloakUserService {
 
     private final Keycloak keycloak;
@@ -33,21 +41,84 @@ public class KeycloakUserServiceImpl implements KeycloakUserService {
 
     @Override
     public UserResponseDTO createUser(UserRequestDTO userRequestDTO) {
-        Response response = createKeycloakUser(userRequestDTO);
-        if (!Objects.equals(201, response.getStatus())) {
-            throw new CustomBadRequestException("Failed to create new user: " + response.getStatusInfo().getReasonPhrase());
+        try {
+            Response response = createKeycloakUser(userRequestDTO);
+            if (!Objects.equals(201, response.getStatus())) {
+                throw new CustomBadRequestException("Failed to create new user: " + response.getStatusInfo().getReasonPhrase());
+            }
+            UserResource currentUser = getCurrentUser(response);
+            UserRepresentation userRepresentation = currentUser.toRepresentation();
+            return userMapper.toUserResponseDTO(userRepresentation);
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            throw new CustomBadRequestException("Cannot create user by error : " + e.getLocalizedMessage());
         }
-        return userMapper.toUserResponseDTO(userRequestDTO);
     }
 
     @Override
-    public UserResponseDTO updateUser(UserRequestDTO userRequestDTO) {
-        return null;
+    public void updateUser(String userId, UserUpdateRequestDTO userUpdateRequestDTO) {
+        if (userId == null) throw new CustomBadRequestException("User id cannot be null.");
+        try {
+            UsersResource usersResource = getUsersResource();
+            UserResource userResource = usersResource.get(userId);
+            UserRepresentation updatedUser = userMapper.updateUser(userUpdateRequestDTO, userResource.toRepresentation());
+            userResource.update(updatedUser);
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            throw new CustomBadRequestException("Failed to update user by error: " + e.getLocalizedMessage());
+        }
+    }
+
+    @Override
+    public UserResponseDTO getUser(String userId) {
+        if (userId == null) throw new CustomBadRequestException("User id cannot be null.");
+        try {
+            UsersResource usersResource = getUsersResource();
+            UserRepresentation userRepresentation = usersResource.get(userId).toRepresentation();
+            return userMapper.toUserResponseDTO(userRepresentation);
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            throw new CustomNotFoundException("User cannot be found by error: " + e.getLocalizedMessage());
+        }
+    }
+
+    @Override
+    public PagingApiResponse<List<UserResponseDTO>> getAllUsers(String search, Integer page, Integer pageSize) {
+        if (page == null || pageSize == null) throw new CustomBadRequestException("page or pageSize cannot be null.");
+        try {
+            Integer skip = pageSize * (page - 1);
+            UsersResource usersResource = getUsersResource();
+            List<UserRepresentation> userRepresentationList = usersResource.search(search, skip, pageSize);
+            Integer totalCount = usersResource.count(search);
+            PageInfo pageInfo = new PageInfo(page, pageSize, Long.parseLong(totalCount.toString()));
+            List<UserResponseDTO> userResponseDTOList = userRepresentationList.stream()
+                    .map(userMapper::toUserResponseDTO)
+                    .collect(Collectors.toList());
+            return new PagingApiResponse<>(HttpStatus.OK.value(), userResponseDTOList, pageInfo);
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            throw new CustomNotFoundException("User cannot be found by error: " + e.getLocalizedMessage());
+        }
+
+    }
+
+    @Override
+    public void deleteUser(String userId) {
+        if (userId == null) throw new CustomBadRequestException("User id cannot be null.");
+
+        try {
+            UsersResource usersResource = getUsersResource();
+            usersResource.delete(userId);
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            throw new CustomBadRequestException("Cannot delete user by error: " + e.getLocalizedMessage());
+        }
+
     }
 
     public Response createKeycloakUser(UserRequestDTO userRequestDTO) {
         if (userRequestDTO.getEmail() == null) throw new CustomBadRequestException("The email is required.");
-        UsersResource usersResource = getUserResource();
+        UsersResource usersResource = getUsersResource();
 
         // Define user
         UserRepresentation userRepresentation = new UserRepresentation();
@@ -71,16 +142,13 @@ public class KeycloakUserServiceImpl implements KeycloakUserService {
         Response response = usersResource.create(userRepresentation);
 
         // Assign roles
-        assignUserRole(response, userRequestDTO.getRole());
+        UserResource currentUser = getCurrentUser(response);
+        assignUserRole(currentUser, userRequestDTO.getRole());
         return response;
     }
 
-    private void assignUserRole(Response response, String role) {
+    private void assignUserRole(UserResource currentUser, String role) {
         RealmResource realmResource = getRealmResource();
-        UsersResource usersResource = getUserResource();
-
-        String userId = CreatedResponseUtil.getCreatedId(response);
-        UserResource currentUser = usersResource.get(userId);
         ClientRepresentation clientRepresentation = realmResource.clients().findByClientId(clientId).get(0);
         RoleRepresentation userRealmRole = realmResource.roles().get(role).toRepresentation();
         RoleRepresentation userClientRole = realmResource.clients().get(clientRepresentation.getId())
@@ -91,5 +159,11 @@ public class KeycloakUserServiceImpl implements KeycloakUserService {
 
     private RealmResource getRealmResource() { return keycloak.realm(realm); }
 
-    private UsersResource getUserResource() { return getRealmResource().users(); }
+    private UsersResource getUsersResource() { return getRealmResource().users(); }
+
+    private UserResource getCurrentUser(Response response) {
+        UsersResource usersResource = getUsersResource();
+        String userId = CreatedResponseUtil.getCreatedId(response);
+        return usersResource.get(userId);
+    }
 }
