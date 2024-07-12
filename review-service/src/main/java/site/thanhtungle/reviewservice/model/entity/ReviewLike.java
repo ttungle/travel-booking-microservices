661c5aafa682dd4ff102fc1dcabb57f137861bf8
Thanh Tung Le <thanhtungle73@gmail.com>
2024-07-12 23:16:49 +0700

    feat(review): added like review api

---
+++ review-service/src/main/java/site/thanhtungle/reviewservice/model/entity/ReviewLike.java
@@ -0,0 +1,27 @@
+package site.thanhtungle.reviewservice.model.entity;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "review_like")
+public class ReviewLike extends BaseEntity {
+
+    @Column(name = "user_id")
+    private String userId;
+
+    @JsonBackReference
+    @ManyToOne(
+            fetch = FetchType.LAZY,
+            cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
+    @JoinColumn(name = "review_id")
+    private Review review;
+}
