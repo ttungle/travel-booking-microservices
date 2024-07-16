3997c1c6f209269db932556ffbc3448cbe4d842b
Thanh Tung Le <thanhtungle73@gmail.com>
2024-07-17 01:06:51 +0700

    feat(api-documentation): centralized api documentation in api gateway

--- api-gateway/src/main/java/site/thanhtungle/apigateway/config/SecurityConfiguration.java
+++ api-gateway/src/main/java/site/thanhtungle/apigateway/config/SecurityConfiguration.java
@@ -10,16 +10,22 @@ import org.springframework.security.web.server.SecurityWebFilterChain;
 
 /**
  * Spring web flux security configuration for API Gateway
- * */
+ */
 @Configuration
 @EnableWebFluxSecurity
 public class SecurityConfiguration {
+
+    private final static String[] AUTH_WHITELIST = {"/eureka/**", "/api/v1/auth/**", "/ws-notification/**",
+            "/webjars/**", "/v3/api-docs/**", "/tour-service/v3/api-docs", "/review-service/v3/api-docs",
+            "/payment-service/v3/api-docs", "/notification-service/v3/api-docs", "/inventory-service/v3/api-docs",
+            "/booking-service/v3/api-docs", "/account-service/v3/api-docs"};
+
     @Bean
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
         http.csrf(ServerHttpSecurity.CsrfSpec::disable)
                 .authorizeExchange(exchange -> exchange
                         .pathMatchers(HttpMethod.GET, "/api/v1/tours/**").permitAll()
-                        .pathMatchers("/eureka/**", "/api/v1/auth/**", "/ws-notification/**").permitAll()
+                        .pathMatchers(AUTH_WHITELIST).permitAll()
                         .anyExchange().authenticated()
                 )
                 .oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults()));
